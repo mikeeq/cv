9d47da16d6136b1458db7cb282d48043acc543dc
mikeeq <miotk.mikolaj@gmail.com>
2026-05-14 16:26:44 +0200

    Add pre-commit

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -5,17 +5,17 @@ set -eu -o pipefail
 DOCKER_IMAGE=fedora_tex:latest
 
 if ! docker images --format "{{.Repository}}:{{.Tag}}" | grep -q ${DOCKER_IMAGE}; then
-  docker build -t ${DOCKER_IMAGE} .
+	docker build -t ${DOCKER_IMAGE} .
 fi
 
 mkdir -p results
 
 docker run \
-  -t \
-  -v "$(pwd)":/repo \
-  -w /repo \
-  ${DOCKER_IMAGE} \
-  /bin/bash -c "
+	-t \
+	-v "$(pwd)":/repo \
+	-w /repo \
+	${DOCKER_IMAGE} \
+	/bin/bash -c "
     pdflatex -output-directory=results mmiotk-cv.tex
   "
 
